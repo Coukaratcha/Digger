49c3bbb3deca963b67addd70d83b75efbfac4d0e
Coukaratcha <mickael.gillot@gmail.com>
2014-04-24 20:37:16 +0200

    Ajout des fichiers sources vides

--- src/main.c
+++ src/main.c
@@ -1,45 +1,6 @@
 #include <SDL/SDL.h>
 
-void pause(void);
-
 int main(int argc, char *argv[])
 {
-    SDL_Surface *ecran = NULL; // Le pointeur qui va stocker la surface de l'écran
-
-    SDL_Init(SDL_INIT_VIDEO);
-
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // On tente d'ouvrir une fenêtre
-    if (ecran == NULL) // Si l'ouverture a échoué, on le note et on arrête
-    {
-        fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
-    SDL_WM_SetCaption("Ma super fenêtre SDL !", NULL);
-
-    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 17, 206, 112));
-
-    SDL_Flip(ecran);
-
-    pause();
-
-    SDL_Quit();
-
     return EXIT_SUCCESS;
-}
-
-void pause()
-{
-    int continuer = 1;
-    SDL_Event event;
-
-    while (continuer)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                continuer = 0;
-        }
-    }
 }
