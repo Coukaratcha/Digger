3874aeb0ba16c679d75738d1b150085d35171fc7
Florian <florian.tissier2@gmail.com>
2014-04-30 12:54:10 +0200

    Creation bloc de fonction dans partie, mode, score et niveau et creation fonction Partie_creer

---
+++ src/Mode.c
@@ -0,0 +1,11 @@
+#include "../include/Mode.h"
+
+Mode* Mode_creer(void)
+{
+    return NULL;
+}
+
+void Mode_chargerRegles(Mode *mode)
+{
+
+}
---
+++ src/Niveau.c
@@ -0,0 +1,17 @@
+#include "../include/Niveau.h"
+
+
+Niveau* Niveau_charger(FILE *fichier)
+{
+    return NULL;
+}
+
+void Niveau_ordonner(Niveau *niveau)
+{
+
+}
+
+void Niveau_afficher(Niveau *niveau)
+{
+
+}
