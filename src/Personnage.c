0686cb51a51ae043ac6eab3fd0155b2b2f88df18
Coukaratcha <mickael.gillot@gmail.com>
2014-05-05 15:42:48 +0200

    Ajout de Personnage_initialiser

--- src/Personnage.c
+++ src/Personnage.c
@@ -2,7 +2,18 @@
 
 void Personnage_initialiser(Personnage *perso, Niveau *niveau)
 {
+	unsigned int i, j;
+	int trouve = 0;
 
+	for (i=0; !trouve; i++) {
+		for (j=0; !trouve; j++) {
+			if (niveau->grille[i][j] == PERSO) {
+				trouve = 1;
+				perso->posx = i*32;
+				perso->posy = BORD_SUP_NIVEAU+j*32;
+			}
+		}
+	}
 }
 
 unsigned int Personnage_seDeplacer(Personnage *perso)
